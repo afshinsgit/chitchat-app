3b66ea76f7beff47e7657eed347557dbd2eaca36
Afshin Safari <afshins.git@gmail.com>
2021-09-16 00:37:53 +0300

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home container">
+    <h3>Home</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+ 
+})
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 15px 0;
+}
+</style>
