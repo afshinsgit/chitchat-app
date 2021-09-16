365374b984a273283656761f1ab6f7cc21d47e63
Afshin Safari <afshins.git@gmail.com>
2021-09-16 17:29:44 +0300

    add login form component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="handleSubmit()">
+    <input type="email" required placeholder="Email" v-model="email">
+    <input type="password" required placeholder="Password" v-model="password">
+    <button>LOG IN</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: "Home",
+  setup() {
+    const email = ref<string>('');
+    const password = ref<string>('');
+
+    function handleSubmit() {
+      console.log(email.value, password.value);
+    }
+
+    return { email, password, handleSubmit }
+  }
+});
+</script>
