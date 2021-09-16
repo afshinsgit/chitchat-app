695301505b950e16b7401c83fea4c43644de4521
Afshin Safari <afshins.git@gmail.com>
2021-09-16 17:36:11 +0300

    Nest login and signup components into home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="home container">
     <h3>Home</h3>
+    <div v-if="showLogin">
+      <h5>Log in to your account</h5>
+      <LoginForm></LoginForm>
+      <hr>
+      <p1>Don't have an account? <span @click="showLogin = false">Sign up</span></p1>
+    </div>
+    <div v-else>
+      <h5>Create an acount</h5>
+      <SignupForm></SignupForm>
+      <hr>
+      <p1>Have an account? <span @click="showLogin = true">Log in</span></p1>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import SignupForm from '../components/SignupForm.vue'
+import LoginForm from '../components/LoginForm.vue'
 
 export default defineComponent({
- 
-})
+ components: { SignupForm, LoginForm },
+ setup() {
+   const showLogin = ref<boolean>(true);
+   
+   return { showLogin }
+ }
+});
 </script>
 
 <style>
