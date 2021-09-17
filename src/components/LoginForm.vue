5d3290ce37cb91eb34e683b2900150ceb9055250
Afshin Safari <afshins.git@gmail.com>
2021-09-17 21:21:59 +0300

    setup login functionality

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,24 +2,32 @@
   <form @submit.prevent="handleSubmit()">
     <input type="email" required placeholder="Email" v-model="email">
     <input type="password" required placeholder="Password" v-model="password">
+    <div class="error">{{ error }}</div>
     <button>LOG IN</button>
   </form>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref } from 'vue'
+import { useLogin } from '../composables/useLogin'
 
 export default defineComponent({
-  name: "Home",
+  name: "LoginForm",
+
   setup() {
+
+    const { error, login } = useLogin();
+
     const email = ref<string>('');
     const password = ref<string>('');
 
-    function handleSubmit() {
-      console.log(email.value, password.value);
+    async function handleSubmit() {
+      await login(email.value, password.value);
+      if(!error.value) {
+        console.log('user logged in!');
+      }
     }
-
-    return { email, password, handleSubmit }
+    return { email, password, error, handleSubmit }
   }
 });
 </script>
---
+++ src/composables/useLogin.ts
@@ -0,0 +1,23 @@
+import { ref } from 'vue'
+import { projectAuth } from '../firebase/firebase'
+
+const error = ref<string | null>(null);
+
+async function login(email: string, password: string) {
+  error.value = null;
+  try {
+    const response = await projectAuth.signInWithEmailAndPassword(email, password);
+    if(!response) {
+      throw new Error('Could not log in');
+    }
+    error.value = null;
+    return response;
+  } catch(err) {
+    console.log(err.message);
+    error.value = err.message;
+  }
+}
+
+export function useLogin() {
+  return { error, login };
+}
