9efd45d5893bcf3f69aaf7d518896e3834e70f3d
Afshin Safari <afshins.git@gmail.com>
2021-09-20 00:11:01 +0300

    add chatroom component

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>
-    <h3>Chatroom</h3>
+  <div class="chat-container">
+    <Navbar></Navbar>
   </div>
 </template>
+
+
+<script lang="ts">
+import { defineComponent, watchEffect } from 'vue'
+import Navbar from '../components/Navbar.vue'
+import { useRouter } from 'vue-router'
+import { getUser } from '../composables/getUser'
+
+export default defineComponent({
+  name: 'Chatroom',
+  components: { Navbar },
+  setup() {
+
+    const router = useRouter();
+    const { currentUser } = getUser();
+
+    watchEffect(() => {
+      if(!currentUser.value) {
+        router.push({name: 'Home'})
+      }
+    });
+
+    return  { currentUser }
+  },
+})
+</script>
+
+
+<style>
+.chat-container {
+  width: 100%;
+  max-width: 1000px;
+  margin: 80px auto;
+  border-radius: 10px;
+  box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.3);
+  color: rgb(0, 0, 0);
+  background: rgb(255, 255, 255);
+}
+
+</style>
