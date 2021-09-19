d5479117af6a50d696077f211cf2b8dcdc38e966
Afshin Safari <afshins.git@gmail.com>
2021-09-20 00:31:34 +0300

    wait for authentication status change and then fire the application so it doesn't redirect the logged in user to the home page.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+import { projectAuth } from './firebase/firebase'
 
-createApp(App).use(router).mount('#app')
+let app: any;
+projectAuth.onAuthStateChanged(() => {
+  if(!app) {
+    app = createApp(App).use(router).mount('#app');
+  }
+});
