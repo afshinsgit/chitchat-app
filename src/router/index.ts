ebc82b4db5361054bed5a5526fc0fb5f47e4bc9b
Afshin Safari <afshins.git@gmail.com>
2021-09-21 19:42:06 +0300

    redirect user to chatroom if already logged in

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,21 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import { projectAuth } from '../firebase/firebase'
 
-
-// Authentication guard
 // @ts-ignore
-function requireAuth(to, from, next) {
+function requireAuthentication(to, from, next) {
   const user = projectAuth.currentUser;
   if(!user) {
     next({ name: 'Home'})
+  } else {
+    next();
+  }
+}
+
+// @ts-ignore
+function requireNoAuthentication(to, from, next) {
+  const user = projectAuth.currentUser;
+  if(user) {
+    next({ name: 'Chatroom'})
   } else {
     next();
   }
@@ -18,13 +26,14 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: requireNoAuthentication
   },
   {
     path: '/chatroom',
     name: 'Chatroom',
     component: () => import('../views/Chatroom.vue'),
-    beforeEnter: requireAuth
+    beforeEnter: requireAuthentication
   }
 ]
 
